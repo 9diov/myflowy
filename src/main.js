e90839e3a90936f0ec9ce338b583767320d0f7ee
Thanh Dinh Khac <thanhdk@gmail.com>
2017-06-21 09:21:42 +0700

    Add store object

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import Store from './store';
 
 new Vue({
+  store: Store,
   el: '#app',
   render: h => h(App)
-})
+});
